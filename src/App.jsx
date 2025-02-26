913d06072b415669fb28d39e38f21518cebc4015
deepanshu <21f3001217@ds.study.iitm.ac.in>
2025-02-26 10:00:18 +0530

    Updated hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Generator from "./components/Generator";
+import Hero from "./components/Hero";
+import Workout from "./components/Workout";
+
 function App() {
 
   return (
-    <>
-      <div>Hello world!</div>
-    </>
+    <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
+      <Hero />
+      <Workout />
+      <Generator />
+    </main>
   )
 }
 
